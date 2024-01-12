e4cadab87576bf373c3becaf9d6eb445cdc8cb8d
Agin Gosh <agin@zfloc.com>
2024-01-13 02:51:38 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,3 @@
+const heading = React.createElement("h1", {style:{color:'red'}}, "My first React App using cdn");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
