f8a9c3c0fb431a00fa174d1833c75663c3c16b4a
Agin Gosh <agin@zfloc.com>
2024-02-03 13:10:58 +0530

    feat: foundation layout

--- App.js
+++ App.js
@@ -1,3 +1,18 @@
-const heading = React.createElement("h1", {style:{color:'red'}}, "My first React App using cdn");
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const Title = () => (
+  <h1 className="head" style={{ color: "blue" }}>
+    Using Jsx
+  </h1>
+);
+
+const FunctionalComponent = () => (
+    <div className="contaner"> 
+    <Title/>
+        <h1 className="heading">React Using Functional Component</h1>
+    </div>
+)
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(<FunctionalComponent/>);
---
+++ dist/index.6bd02f5a.js
@@ -0,0 +1,9 @@
+const heading = React.createElement("h1", {
+    style: {
+        color: "red"
+    }
+}, "My first React App using cdn");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
+
+//# sourceMappingURL=index.6bd02f5a.js.map
